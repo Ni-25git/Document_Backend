5b75ef90b31cd52f57dbb37a3bf216c779636e8e
Ni-25git <nipunsherawat@gmail.com>
2025-07-11 09:17:46 +0530

    kkk

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(cors({
     origin: "https://document-frontend-6m93.vercel.app",
     credentials: true,
-    
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
 }));
 app.use('/api/user' , user);
 app.use('/api/doc',document)
