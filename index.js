9b6cacb890a278eaa48969aa2d190b9ef449ef49
Ni-25git <nipunsherawat@gmail.com>
2025-07-11 11:45:24 +0530

    Document Backend

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors({
-    origin: ["https://document-frontend-6m93-b07wog21s-nipun-sehrawat-projects.vercel.app/login", "https://document-frontend-6m93.vercel.app"],
+    origin: ["https://document-frontend-6m93-b07wog21s-nipun-sehrawat-projects.vercel.app", "https://document-frontend-6m93.vercel.app"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
