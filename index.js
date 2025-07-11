38de67acbd0e1dea3c06805b4c48ef098854af6e
Ni-25git <nipunsherawat@gmail.com>
2025-07-11 08:29:09 +0530

    Document Backend

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',"https://document-frontend-6m93.vercel.app"],
     credentials: true,
 }));
 app.use('/api/user' , user);
